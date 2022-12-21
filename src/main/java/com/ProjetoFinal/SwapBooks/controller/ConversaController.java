e162eb1c2b48731d5fdfc41ea26341e42d5484bd
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-12-21 14:16:30 -0300

    teste

--- src/main/java/com/ProjetoFinal/SwapBooks/controller/ConversaController.java
+++ src/main/java/com/ProjetoFinal/SwapBooks/controller/ConversaController.java
@@ -1,13 +1,12 @@
 package com.ProjetoFinal.SwapBooks.controller;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.ProjetoFinal.SwapBooks.model.Conversa;
 import com.ProjetoFinal.SwapBooks.repository.ConversaRepository;
 
 @Controller
@@ -22,8 +21,7 @@ public class ConversaController {
     }
     @GetMapping("/conversa")
     public String relatorio(Model model){
-       List<Conversa> lista = convRepo.findAll();
-       model.addAttribute("conversa",lista);
+       model.addAttribute("conversa",convRepo.findAll());
        return "conversa";
     }
     
