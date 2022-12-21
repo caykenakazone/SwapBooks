68c213d935727aa94bfbde09741198b9ea7c87da
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-12-21 15:27:47 -0300

    correção bugs

--- src/main/java/com/ProjetoFinal/SwapBooks/controller/ConversaController.java
+++ src/main/java/com/ProjetoFinal/SwapBooks/controller/ConversaController.java
@@ -1,21 +1,15 @@
 package com.ProjetoFinal.SwapBooks.controller;
 
 
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.ProjetoFinal.SwapBooks.repository.ConversaRepository;
+
 
 @Controller
 public class ConversaController {
 
-    @Autowired
-    private ConversaRepository convRepo;
 
     @GetMapping("/chat")
     public String home(){
--- src/main/java/com/ProjetoFinal/SwapBooks/repository/ConversaRepository.java
+++ src/main/java/com/ProjetoFinal/SwapBooks/repository/ConversaRepository.java
@@ -1,14 +1,12 @@
 package com.ProjetoFinal.SwapBooks.repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.ProjetoFinal.SwapBooks.model.Conversa;
+
 
 
 @Repository
